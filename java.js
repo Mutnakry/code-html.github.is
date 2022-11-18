b508f736bee7adbee5712f7cba33260db8a18915
Mutnakry <118255402+Mutnakry@users.noreply.github.com>
2022-11-18 15:39:21 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+​document.getElementById("b").style.backgroundColor ="red";
+document.getElementById("b").style.background ="blue";
+document.getElementById("a").style.backgroundColor ="red";
+document.getElementById("a").style.background ="blue";
+
+function textStyle() {
+    text.style.background="pink";
+    text.style.color="blue";
+    text.style.fontSize="2em";
+}
+function textStyle1() {
+    text.style.border="10px solid blue";
+    text.style.padding="20px";
+   
+}
+function eraseTextStyle(){
+    text.style.background="";
+    text.style.color="";
+    text.style.fontSize="";
+    text.style.border="";
+    text.style.padding="";
+}
+
+
+
